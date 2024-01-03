92e7b3ba9e09a6e80d66dba2468fa09910508653
qwe321qwe321qwe321 <aegis321123@gmail.com>
2024-01-03 10:40:42 +0800

    Added profiler tags for TransfromAccessArrayTest

--- Assets/GpuSplines/Test/TransformAccessArrayTest.cs
+++ Assets/GpuSplines/Test/TransformAccessArrayTest.cs
@@ -9,6 +9,7 @@ using Unity.Jobs;
 using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Jobs;
+using UnityEngine.Profiling;
 using Random = Unity.Mathematics.Random;
 
 namespace PeDev {
@@ -22,8 +23,8 @@ namespace PeDev {
 	    public float splineWidthRandomMin = 0.1f;
 	    public float splineWidthRandomMax = 0.5f;
 
-	    private List<GameObject> objectLists = new List<GameObject>();
-	    private TransformAccessArray transformArray;
+	    private readonly List<GameObject> m_ObjectLists = new List<GameObject>();
+	    private TransformAccessArray m_TransformAccessArray;
 	    private JobHandle m_JobHandle;
 	    private MoveUpdateJob m_MoveUpdateJob;
 	    private Random m_Random;
@@ -40,10 +41,10 @@ namespace PeDev {
 	    }
 
 	    private void Start() {
-		    if (transformArray.isCreated) {
-			    transformArray.Dispose();
+		    if (m_TransformAccessArray.isCreated) {
+			    m_TransformAccessArray.Dispose();
 		    }
-		    transformArray = new TransformAccessArray(objectCount);
+		    m_TransformAccessArray = new TransformAccessArray(objectCount);
 		    m_SharedPositions = new SharedArray<Vector3, float3>(objectCount);
 		    Vector3[] positions = m_SharedPositions;
 		    for (int i = 0; i < objectCount; i++) {
@@ -51,8 +52,8 @@ namespace PeDev {
 			    newObject.name = i.ToString();
 			    newObject.transform.SetParent(this.transform);
 			    newObject.transform.localPosition = UnityEngine.Random.insideUnitSphere * initialRadius;
-			    objectLists.Add(newObject);
-			    transformArray.Add(newObject.transform);
+			    m_ObjectLists.Add(newObject);
+			    m_TransformAccessArray.Add(newObject.transform);
 			    positions[i] = newObject.transform.position;
 		    }
 
@@ -103,23 +104,26 @@ namespace PeDev {
 
 	    private void OnDestroy() {
 		    m_Splines.Dispose();
+		    m_TransformAccessArray.Dispose();
 	    }
 
 	    private void Update() {
 		    if (!m_JobHandle.IsCompleted) {
 			    m_JobHandle.Complete();
 		    }
+		    Profiler.BeginSample("Update Jobs"); 
 
+		    Profiler.BeginSample("Job Schedules");
 		    if (randomMove) {
 			    m_JobHandle = new MoveUpdateJob() {
 				    random = m_Random, 
 				    deltaTime = Time.deltaTime * moveSpeed,
-			    }.Schedule(transformArray);
+			    }.Schedule(m_TransformAccessArray);
 		    }
 
 		    m_JobHandle = new CopyTransformToArrayJob() {
 			    nativePositions = m_SharedPositions
-		    }.Schedule(transformArray, m_JobHandle);
+		    }.Schedule(m_TransformAccessArray, m_JobHandle);
 
 		    var splineContextJobified = m_Context.BeginJobifiedContext(Allocator.TempJob);
 		    m_JobHandle = new UpdateSplineJob() {
@@ -128,11 +132,17 @@ namespace PeDev {
 			    insertFirstLastPoints = true,
 			    splineContext = splineContextJobified,
 		    }.Schedule(m_Splines.Length, 4, m_JobHandle);
+		    Profiler.EndSample();
+
 		    m_JobHandle.Complete();
 		    m_Context.EndJobifiedContext(splineContextJobified);
+		    Profiler.EndSample();
+
 		    
+		    Profiler.BeginSample("Spline Update"); 
 		    m_Context.Update();
-		    
+		    Profiler.EndSample();
+
 		    m_Random.NextUInt();
 	    }
 	    
